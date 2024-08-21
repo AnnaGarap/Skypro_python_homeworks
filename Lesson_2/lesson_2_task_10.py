bb9fa3d42ee468cda8365bc80773f431d8e587fb
AnnaGarap <anne.garap@gmail.com>
2024-08-22 01:30:55 +0300

    Задания по уроку 2

--- Lesson_2/lesson_2_task_10.py
+++
@@ -1,8 +0,0 @@
-x = int(input("Введите первоначальную сумму вклада: "))
-y = int(input("На сколько лет открывается вклад: "))
-def bank(x, y):
-    for time in range(y):
-        x = x * 1.1
-    print(round(x, 2))
-
-bank(x, y)
--- Lesson_2/lesson_2_task_3.py
+++
@@ -1,8 +0,0 @@
-import math
-
-
-side = float(input("Сторона квадрата: "))
-def square(side):
-    return math.ceil(side*side)
-result = square(side)
-print(result)
