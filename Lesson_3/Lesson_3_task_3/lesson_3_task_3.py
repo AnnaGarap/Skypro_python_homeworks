ca76f8304dfb8b7a7c0661eb0f1b6a9d06a34e52
AnnaGarap <anne.garap@gmail.com>
2024-09-02 00:15:29 +0300

    Update lesson_3_task_3.py
    Исправлена ошибка

--- Lesson_3/Lesson_3_task_3/lesson_3_task_3.py
+++ Lesson_3/Lesson_3_task_3/lesson_3_task_3.py
@@ -5,9 +5,10 @@ to_address = Address("249030", "Обнинск", "Курчатова", "1", "1")
 from_address = Address("119180", "Москва", "Большая Полянка", "3/9", "50")
 mailing = Mailing(to_address, from_address, 350, "150748986449879")
 
-print(f"""Отправление {mailing.track} из {mailing.from_address.index}, "
+print(
+    f"Отправление {mailing.track} из {mailing.from_address.index}, "
     f"{mailing.from_address.city}, {mailing.from_address.street}, "
     f"{mailing.from_address.house} - {mailing.from_address.apartment} "
     f"в {mailing.to_address.index}, {mailing.to_address.city}, "
     f"{mailing.to_address.street}, {mailing.to_address.house} - "
-    f"{mailing.to_address.apartment}. Стоимость {mailing.cost} рублей.""")
+    f"{mailing.to_address.apartment}. Стоимость {mailing.cost} рублей.")
